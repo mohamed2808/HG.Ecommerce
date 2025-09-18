3ce54c7a2fb8f9e76a603d4dae3508607d731a68
Hagag <mohamedabohagag54@gmail.com>
2025-09-18 18:05:32 +0300

    Create ApplicationUser Entity

---
+++ HG.Ecommerce.Core/Entites/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+namespace HG.Ecommerce.Core.Entites
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? Country { get; set; }
+        public string? PostalCode { get; set; }
+        public string Role { get; set; } = "User";
+    }
+}
