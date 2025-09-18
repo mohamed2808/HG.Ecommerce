80baa2d3cb171e1a6a4dd137065aba70bd9caa38
Hagag <mohamedabohagag54@gmail.com>
2025-09-18 21:59:36 +0300

    Implement Generic Repository Design Pattern

---
+++ HG.Ecommerce.Core/Contracts/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using HG.Ecommerce.Core.Common;
+namespace HG.Ecommerce.Core.Contracts
+{
+    public interface IGenericRepository<TEntity, TKey> where TEntity : BaseEntity<TKey> where TKey : IEquatable<TKey>
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync(bool withTracking = false);
+        Task<TEntity?> GetByIdAsync(TKey id);
+        Task AddAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task DeleteAsync(TKey id);
+    }
+}
---
+++ HG.Ecommerce.Infrastruction.Presistance/Data/Presistance/Generic Repository/GenericRepository.cs
@@ -0,0 +1,40 @@
+﻿using HG.Ecommerce.Core.Common;
+using HG.Ecommerce.Core.Contracts;
+using HG.Ecommerce.Infrastruction.Presistance.Data.DbContextFile;
+using Microsoft.EntityFrameworkCore;
+namespace HG.Ecommerce.Infrastruction.Presistance.Data.Presistance.Generic_Repository
+{
+    public class GenericRepository<TEntity, TKey> : IGenericRepository<TEntity, TKey>
+        where TEntity : BaseEntity<TKey>
+        where TKey : IEquatable<TKey>
+    {
+        private readonly EcommerceDbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+        public GenericRepository(EcommerceDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+
+            _dbSet = context.Set<TEntity>();
+        }
+        public async Task<IEnumerable<TEntity>> GetAllAsync(bool withTracking = false)
+        => withTracking
+            ? await _context.Set<TEntity>().ToListAsync()
+            : await _context.Set<TEntity>().AsNoTracking().ToListAsync();
+
+        public Task<TEntity?> GetByIdAsync(TKey id)
+      => _context.Set<TEntity>().FirstOrDefaultAsync(e => e.Id.Equals(id));
+
+        public async Task AddAsync(TEntity entity)
+        {
+            await _context.Set<TEntity>().AddAsync(entity);
+        }
+        public async Task UpdateAsync(TEntity entity)
+        {
+            _context.Set<TEntity>().Update(entity);
+        }
+        public async Task DeleteAsync(TKey id)
+        {
+            await _context.Set<TEntity>().Where(e => e.Id.Equals(id)).ExecuteDeleteAsync();
+        }
+    }
+}
