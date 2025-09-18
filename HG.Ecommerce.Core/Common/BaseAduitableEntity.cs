656b664f88339797d82537a310e07cbe921c6e2a
Hagag <mohamedabohagag54@gmail.com>
2025-09-18 18:02:12 +0300

    Create Product and Category Entites

---
+++ HG.Ecommerce.Core/Common/BaseAduitableEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace HG.Ecommerce.Core.Common
+{
+    public class BaseAduitableEntity<TKey> : BaseEntity<TKey> where TKey : IEquatable<TKey>
+    {
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public string? CreatedBy { get; set; }
+        public string? UpdatedBy { get; set; }
+        public bool? IsDeleted { get; set; } = false;
+        public DateTime? DeletedAt { get; set; }
+        public string? DeletedBy { get; set; }
+    }
+}
---
+++ HG.Ecommerce.Core/Common/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace HG.Ecommerce.Core.Common
+{
+    public class BaseEntity<TKey> where TKey : IEquatable<TKey>
+    {
+        public TKey Id { get; set; }
+    }
+}
