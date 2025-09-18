32ed85bd3b104748e5ec7a4c0fa6d23b976fd9c6
Hagag <mohamedabohagag54@gmail.com>
2025-09-18 22:07:51 +0300

    Implement Unit Of Work Design Pattern

---
+++ HG.Ecommerce.Core/Contracts/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using HG.Ecommerce.Core.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HG.Ecommerce.Core.Contracts
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>()
+            where TEntity : BaseEntity<TKey>
+            where TKey : IEquatable<TKey>;
+        Task<int> CompleteAsync();
+    }
+}
